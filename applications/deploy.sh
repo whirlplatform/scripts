d84a35ea6fc68a54b6f656930fd496bba8fa4a34
Pavel Semenov <otlichnosti@gmail.com>
2019-04-03 17:33:55 +0300

    jelastic deploy script

--- applications/deploy.sh
+++ applications/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo 'Deploy started'
-find . ! -path './.*' ! -path './scripts*' -type f -exec curl --user $JELASTIC_FTP_USER:$JELASTIC_FTP_PASSWORD --ftp-create-dirs -T {} ftp://$JELASTIC_TOMCAT_IP/$JELASTIC_APPLICATION_PATH/{} \;
+find . ! -path './.*' ! -path './scripts*' -type f -exec curl -v --user $JELASTIC_FTP_USER:$JELASTIC_FTP_PASSWORD --ftp-create-dirs -T {} ftp://$JELASTIC_TOMCAT_IP/$JELASTIC_APPLICATION_PATH/{} \;
 echo 'Deploying complete'
