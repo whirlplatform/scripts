310e107edbeb26d6816e4a1532ddf0bfe9815b5b
Pavel Semenov <pavel.semenov@domru.ru>
2022-07-27 16:10:57 +0300

    CI fix.

--- whirl/deploy-jelastic.sh
+++ whirl/deploy-jelastic.sh
@@ -2,8 +2,20 @@
 
 echo 'Jelastic: Application deploying...'
 cd ~/build/whirl-app/whirl-app-server && mvn jelastic:deploy -P deploy-jelastic -Djelastic.hoster_api=$JELASTIC_HOSTER_API -Djelastic.login=$JELASTIC_LOGIN -Djelastic.password=$JELASTIC_PASSWORD -Djelastic.environment=$JELASTIC_ENVIRONMENT  -Djelastic.context=$JELASTIC_CONTEXT_APP
-echo 'Jelastic: Application deploying completed'
+if [ $? -eq 0 ]
+then
+  echo 'Jelastic: Application deploying completed'
+else
+  echo 'FAIL: Jelastic deployment '
+  exit 1
+fi
 
 echo 'Jelastic: Editor deploying...'
 cd ~/build/whirl-editor/whirl-editor-server && mvn jelastic:deploy -P deploy-jelastic -Djelastic.hoster_api=$JELASTIC_HOSTER_API -Djelastic.login=$JELASTIC_LOGIN -Djelastic.password=$JELASTIC_PASSWORD -Djelastic.environment=$JELASTIC_ENVIRONMENT  -Djelastic.context=$JELASTIC_CONTEXT_EDITOR
-echo 'Jelastic: Editor deploying completed'
+if [ $? -eq 0 ]
+then
+  echo 'Jelastic: Editor deploying completed'
+else
+  echo 'FAIL: Jelastic deployment '
+  exit 1
+fi
