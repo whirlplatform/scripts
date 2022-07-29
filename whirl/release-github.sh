609ce3bf4e82006d3dd290749dcbb2b2a096eef2
Pavel Semenov <pavel.semenov@domru.ru>
2022-07-29 17:38:10 +0300

    CI/CD

--- whirl/release-github.sh
+++ whirl/release-github.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
+
+PROJECT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+GIT_BRANCH=$SCRUTINIZER_BRANCH
+
+# Creating tag and github release.
 curl \
   -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: token $GITHUB_ACCESS_TOKEN" \
   https://api.github.com/repos/whirlplatform/whirl/releases \
-  -d '{"tag_name":"'"$SCRUTINIZER_BRANCH"'","name":"'"$SCRUTINIZER_BRANCH"'","body":"Release version: '"$SCRUTINIZER_BRANCH"'","draft":false,"prerelease":false,"generate_release_notes":false}'
+  -d '{"tag_name":"'"$PROJECT_VERSION"'","target_commitish":"'"$GIT_BRANCH"'","name":"'"$PROJECT_VERSION"'","body":"Release '"$PROJECT_VERSION"'","draft":false,"prerelease":false,"generate_release_notes":false}'
