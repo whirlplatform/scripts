cd0bb254464479f1aaa2913f49d9856bb2381b73
Pavel Semenov <pavel.semenov@domru.ru>
2022-10-20 16:32:27 +0300

    CI/CD

--- whirl/deploy-jelastic-cli.sh
+++ whirl/deploy-jelastic-cli.sh
@@ -1,31 +1,51 @@
+#!/usr/bin/env bash
+# This script deploys the application and editor to the Jelastic cloud from github.com release page:
+# https://github.com/whirlplatform/whirl/releases
+#
+# Environment variables:
+#   JELASTIC_HOSTER_API - Jelastic hoster API URL
+#   JELASTIC_LOGIN - Jelastic user name
+#   JELASTIC_PASSWORD - Jelastic password
+#   TAG - version of the application to deploy
+#   ENV_PREFIX - prefix of the environment name
+#   ENV_TYPE - type of the environment to deploy to. Possible values: demo, develop
+#   ENV_CONTEXT - context on the server to deploy to
+
+JELASTIC_ENVIRONMENT="${ENV_PREFIX}-${ENV_TYPE}"
+
+if [ -z "$ENV_CONTEXT" ]
+then
+  ENV_CONTEXT_APP=ROOT
+  ENV_CONTEXT_EDITOR=editor
+else
+  ENV_CONTEXT_APP=$ENV_CONTEXT
+  ENV_CONTEXT_EDITOR=$ENV_CONTEXT-editor
+fi
 
 curl -s ftp://ftp.jelastic.com/pub/cli/jelastic-cli-installer.sh | bash
 
 ~/jelastic/users/authentication/signin --login $JELASTIC_LOGIN --password $JELASTIC_PASSWORD --platformUrl $JELASTIC_HOSTER_API --silent true
-if [ $? -eq 0 ]
-then
-  echo 'Jelastic: Application deploying completed'
-else
-  echo 'FAIL: Jelastic deployment '
+if [ $? -ne 0 ]; then
+  echo "Jelastic: Failed to sign in"
   exit 1
 fi
 
 echo 'Jelastic: Application deploying...'
-~/jelastic/environment/deployment/deployarchive --envName whirl-demo --context ROOT --fileUrl https://github.com/whirlplatform/whirl/releases/download/v0.1.0/whirl-application-v0.1.0.war --fileName whirl-application-v0.1.0.war
+~/jelastic/environment/deployment/deployarchive --envName ${JELASTIC_ENVIRONMENT} --context ${ENV_CONTEXT_APP} --fileUrl "https://github.com/whirlplatform/whirl/releases/download/${TAG}/whirl-application-${TAG}.war" --fileName whirl-application-${TAG}.war
 if [ $? -eq 0 ]
 then
-  echo 'Jelastic: Application deploying completed'
+  echo 'Jelastic: Application deployment completed'
 else
-  echo 'FAIL: Jelastic deployment '
+  echo 'FAIL: Deployment of application to Jelastic'
   exit 1
 fi
 
 echo 'Jelastic: Editor deploying...'
-cd ~/build/whirl-editor/whirl-editor-server && mvn jelastic:deploy -P deploy-jelastic -Djelastic.hoster_api=$JELASTIC_HOSTER_API -Djelastic.login=$JELASTIC_LOGIN -Djelastic.password=$JELASTIC_PASSWORD -Djelastic.environment=$JELASTIC_ENVIRONMENT  -Djelastic.context=$JELASTIC_CONTEXT_EDITOR
+~/jelastic/environment/deployment/deployarchive --envName ${JELASTIC_ENVIRONMENT} --context ${ENV_CONTEXT_EDITOR} --fileUrl "https://github.com/whirlplatform/whirl/releases/download/${TAG}/whirl-editor-${TAG}.war" --fileName whirl-editor-${TAG}.war
 if [ $? -eq 0 ]
 then
-  echo 'Jelastic: Editor deploying completed'
+  echo 'Jelastic: Editor deployment completed'
 else
-  echo 'FAIL: Jelastic deployment '
+  echo 'FAIL: Deployment of editor to Jelastic'
   exit 1
 fi
--- whirl/deploy-jelastic-maven.sh
+++ whirl/deploy-jelastic-maven.sh
@@ -1,21 +1,34 @@
 #!/usr/bin/env bash
+# This script deploys the application and editor to the Jelastic cloud with Jelastic Maven plugin.
+# It deploys the application and editor using project's packaged war files.
+#
+# Environment variables:
+#   JELASTIC_HOSTER_API - Jelastic hoster API URL
+#   JELASTIC_LOGIN - Jelastic user name
+#   JELASTIC_PASSWORD - Jelastic password
+#   TAG - version of the application to deploy
+#   ENV_PREFIX - prefix of the environment name
+#   ENV_TYPE - type of the environment to deploy to. Possible values: demo, develop
+#   ENV_CONTEXT - context on the server to deploy to
+
+JELASTIC_ENVIRONMENT="${ENV_PREFIX}-${ENV_TYPE}"
 
 echo 'Jelastic: Application deploying...'
-cd ~/build/whirl-app/whirl-app-server && mvn jelastic:deploy -P deploy-jelastic -Djelastic.hoster_api=$JELASTIC_HOSTER_API -Djelastic.login=$JELASTIC_LOGIN -Djelastic.password=$JELASTIC_PASSWORD -Djelastic.environment=$JELASTIC_ENVIRONMENT  -Djelastic.context=$JELASTIC_CONTEXT_APP
+cd ~/build/whirl-app/whirl-app-server && mvn jelastic:deploy -P deploy-jelastic -Djelastic.hoster_api=$JELASTIC_HOSTER_API -Djelastic.login=$JELASTIC_LOGIN -Djelastic.password=$JELASTIC_PASSWORD -Djelastic.environment=$JELASTIC_ENVIRONMENT  -Djelastic.context=${ENV_CONTEXT}
 if [ $? -eq 0 ]
 then
-  echo 'Jelastic: Application deploying completed'
+  echo 'Jelastic: Application deployment completed'
 else
-  echo 'FAIL: Jelastic deployment '
+  echo 'FAIL: Deployment of application to Jelastic'
   exit 1
 fi
 
 echo 'Jelastic: Editor deploying...'
-cd ~/build/whirl-editor/whirl-editor-server && mvn jelastic:deploy -P deploy-jelastic -Djelastic.hoster_api=$JELASTIC_HOSTER_API -Djelastic.login=$JELASTIC_LOGIN -Djelastic.password=$JELASTIC_PASSWORD -Djelastic.environment=$JELASTIC_ENVIRONMENT  -Djelastic.context=$JELASTIC_CONTEXT_EDITOR
+cd ~/build/whirl-editor/whirl-editor-server && mvn jelastic:deploy -P deploy-jelastic -Djelastic.hoster_api=$JELASTIC_HOSTER_API -Djelastic.login=$JELASTIC_LOGIN -Djelastic.password=$JELASTIC_PASSWORD -Djelastic.environment=$JELASTIC_ENVIRONMENT  -Djelastic.context="${ENV_CONTEXT}-editor"
 if [ $? -eq 0 ]
 then
-  echo 'Jelastic: Editor deploying completed'
+  echo 'Jelastic: Editor deployment completed'
 else
-  echo 'FAIL: Jelastic deployment '
+  echo 'FAIL: Deployment of editor to Jelastic'
   exit 1
 fi
